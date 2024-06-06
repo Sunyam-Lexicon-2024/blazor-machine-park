b6a68f3f2aef94734af749304fa048bd15092f0b
suny-am <visualarea.1@gmail.com>
2024-06-06 20:47:59 +0000

    Add MudBlazor

--- MachinePark.UI/Program.cs
+++ MachinePark.UI/Program.cs
@@ -1,10 +1,12 @@
 using MachinePark.UI.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddMudServices()
+                .AddRazorComponents()
+                .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
@@ -15,6 +17,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 
