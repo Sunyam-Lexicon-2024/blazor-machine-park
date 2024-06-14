7b90fc9e462b87c930865eda14c05cdddd60dbf5
suny-am <visualarea.1@gmail.com>
2024-06-14 14:01:26 +0000

    propagate database update event notification to blazor components

--- MachinePark.UI/EventHandlers/OnMachineDataUpdated.cs
+++ MachinePark.UI/EventHandlers/OnMachineDataUpdated.cs
@@ -1,20 +1,18 @@
 using FastEndpoints;
 using MachinePark.Core.Events;
+using MachinePark.UI.Services;
 
 namespace MachinePark.UI.EventHanders;
 
-internal class OnMachineDataUpdated(ILogger<OnMachineDataUpdated> logger) : IEventHandler<MachineDataUpdated>
+internal class OnMachineDataUpdated(ILogger<OnMachineDataUpdated> logger, INotificationService notificationService) : IEventHandler<MachineDataUpdated>
 {
     private readonly ILogger<OnMachineDataUpdated> _logger = logger;
-
-    public event Action OnChange;
+    private readonly INotificationService _notificationService = notificationService;
 
     public Task HandleAsync(MachineDataUpdated eventModel, CancellationToken ct)
     {
         _logger.LogInformation("{number} - {description}", eventModel.Id, eventModel.Description);
-        NotifyDataUpdated();
+        _notificationService.DispatchNotification();
         return Task.CompletedTask;
     }
-
-    private void NotifyDataUpdated() => OnChange?.Invoke();
 }
